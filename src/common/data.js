5ba07523b150a23d7304348fc1e5c6517897fafe
Liam <liam.isle@gmail.com>
2022-06-11 12:04:50 +1000

    Data file added

---
+++ src/common/data.js
@@ -0,0 +1,26 @@
+export const data = [
+  {
+    handle: "nathanf",
+    socialMediaPlatform: "facebook",
+    followers: 1987,
+    todayFollowerChange: 12,
+  },
+  {
+    handle: "nathanf",
+    socialMediaPlatform: "twitter",
+    followers: 1044,
+    todayFollowerChange: 99,
+  },
+  {
+    handle: "realnathanf",
+    socialMediaPlatform: "instagram",
+    followers: 11000,
+    todayFollowerChange: 1099,
+  },
+  {
+    handle: "Nathan F",
+    socialMediaPlatform: "youtube",
+    followers: 8239,
+    todayFollowerChange: 144,
+  },
+];
