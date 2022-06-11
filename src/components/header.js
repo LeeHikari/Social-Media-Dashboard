14ffc29d45fcce3160188de33929c916f56ce5ab
Liam <liam.isle@gmail.com>
2022-06-11 12:05:08 +1000

    Added css to Header

--- src/components/header.js
+++ src/components/header.js
@@ -12,12 +12,19 @@ export function Header() {
 const HeaderDiv = styled.div`
   padding-left: 100px;
   padding-right: 100px;
+  padding-top: 25px;
+  padding-bottom: 25px;
+  background-color: #f3f6f4;
 `;
 
 const Title = styled.h1`
+  font-size: 24px;
   margin-bottom: 0px;
+  margin-top: 0px;
 `;
 
 const FollowerCount = styled.span`
+  font-weight: bold;
+  font-size: 14px;
   opacity: 0.5;
 `;
