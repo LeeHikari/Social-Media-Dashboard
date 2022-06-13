177023e065e2e5505317a3feede868e521e1054c
Liam Isle <liam.isle@gmail.com>
2022-06-13 13:17:05 +1000

    Added prop to count total followers

--- src/App.js
+++ src/App.js
@@ -3,11 +3,17 @@ import "./App.css";
 import styled from "styled-components";
 import { Header } from "./components/header";
 import { DisplaySocialMediaView } from "./components/SocialMediaDashboard/index";
+import { data } from "./common/data";
 
 function App() {
+  let totalFollowers = 0;
+  for (let i = 0; i < data.length; i++) {
+    totalFollowers += data[i].followers;
+  }
+
   return (
     <div>
-      <Header />
+      <Header totalFollowers={totalFollowers} />
       <Layout>
         <DisplaySocialMediaView />
       </Layout>
